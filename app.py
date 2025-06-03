7f55955fb63ccebe6f275a5c8c9717ac1efbc74a
MikeCrazft <michaelchowkinwai@gmail.com>
2025-06-03 23:49:24 +0900

    flaskapp + freeze

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+from flask_frozen import Freezer
+
+app = Flask(__name__)
+freezer = Freezer(app)
+app.config['FREEZER_DESTINATION'] = 'output'
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1 and sys.argv[1] == "freeze":
+        freezer.freeze()
+    else:
+        app.run(debug=True)
